5ed230d7e70cae28543e7b8add2c8969c0360b64
zunema <yxxa.abc@gmail.com>
2023-01-18 23:47:42 +0900

    モデル実装

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,10 @@
+from datetime import timezone
 from django.db import models
 
-# Create your models here.
+class Diary (models, Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateField(verbose_name='日付', default=timezone.now)
+    title = models.CharField(verbose_name='タイトル', max_length=40)
+    text = models.CharField(verbose_name='本文', max_length=200)
+    created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
+    updated_at = models.DateTimeField(verbose_name='編集日時', blank=True, null=True)
