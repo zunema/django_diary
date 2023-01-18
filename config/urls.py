c8df01cfad179f48d0872d53c52a9283c81d6200
zunema <yxxa.abc@gmail.com>
2023-01-18 23:30:10 +0900

    index表示

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path  # 追加：includeをimport
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('diary.urls')),  # 追加：includeメソッドでurl設定を追加
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,5 @@
 from django.views.generic import TemplateView
- 
-class IndexView(TemplateView):
-    template_name = 'index.html'from django.shortcuts import render
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
