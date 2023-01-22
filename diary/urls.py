3d3d5ee80423f948df36f2cb1037941920ad85f9
zunema <yxxa.abc@gmail.com>
2023-01-22 18:07:49 +0900

    formパス追加

--- diary/urls.py
+++ diary/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'diary'
 urlpatterns = [
     path('index/', views.IndexView.as_view(), name='index'),
+    path('diary/create/', views.DiaryCreateView.as_view(), name='diary_create'),
+    path('diary/create/complete/', views.DiaryCreateCompleteView.as_view(), name='diary_create_complete'),
 ]
