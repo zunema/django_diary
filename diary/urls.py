c20d952776eb73295bae3ab042765732aa88141b
zunema <yxxa.abc@gmail.com>
2023-01-22 23:20:27 +0900

    編集機能の実装

--- diary/urls.py
+++ diary/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('diary/create/', views.DiaryCreateView.as_view(), name='diary_create'),
     path('diary/create/complete/', views.DiaryCreateCompleteView.as_view(),name='diary_create_complete'),
     path('diary/list/', views.DiaryListView.as_view(), name='diary_list'),
+    path('diary/detail/<uuid:pk>/', views.DiaryDetailView.as_view(), name='diary_detail'),
+    path('diary/update/<uuid:pk>/', views.DiaryUpdateView.as_view(), name='diary_update'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,10 +1,14 @@
 from django.views.generic import TemplateView
 from django.views.generic import CreateView
 from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
 from django.urls import reverse_lazy
 
 from diary.forms import DiaryForm
 from diary.forms import Diary
+from django.utils import timezone
 
 
 class IndexView(TemplateView):
@@ -20,6 +24,30 @@ class DiaryCreateView(CreateView):
 class DiaryCreateCompleteView(TemplateView):
     template_name = 'diary_create_complete.html'
 
+
 class DiaryListView(ListView):
     template_name = 'diary_list.html'
     model = Diary
+
+
+class DiaryDetailView(DetailView):
+    template_name = 'diary_detail.html'
+    model = Diary
+
+
+class DiaryUpdateView(UpdateView):
+    template_name = 'diary_update.html'
+    model = Diary
+    fields = ('date', 'title', 'text')
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.update_at = timezone.now()
+        diary.save()
+        return super().form_valid(form)
+
+class DiaryDeleteView(DeleteView):
+    template_name = 'diary_delete.html'
+    model = Diary
+    success_url = reverse_lazy('diary:diary_list')
